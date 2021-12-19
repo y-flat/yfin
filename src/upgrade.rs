9ce7dea5064c78bf41788c5d87d4c02f381411e9
Cameron <cjpuffnstuff@gmail.com>
2021-12-18 21:07:49 -0800

    Add ability to grab manifests from repos

--- src/upgrade.rs
+++ src/upgrade.rs
@@ -4,5 +4,5 @@ use crate::package::get_package_url;
 pub fn upgrade(package: &str) {
     // Get url from package name
     let package_url = get_package_url(package);
-    install(package_url)
+    install(package_url);
 }
