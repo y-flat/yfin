21affae009ba25031a813e046eee2f07b9854f1c
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-12-20 12:35:12 -0800

    Add tests for common

--- src/common.rs
+++ src/common.rs
@@ -5,3 +5,20 @@ pub fn https_prefix(url: &str) -> String {
 pub fn github_prefix(url: &str) -> String {
     return https_prefix(format!("github.com/{}", url).as_str());
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn https_prefix_test() {
+        assert_eq!(https_prefix("this.com"), "https://this.com");
+        assert_ne!(https_prefix("this.com"), "https://that.com");
+    }
+
+    #[test]
+    fn github_prefix_test() {
+        assert_eq!(github_prefix("some/thing"), "https://github.com/some/thing");
+        assert_ne!(github_prefix("some/thing"), "https://that.com");
+    }
+}
