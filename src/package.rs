d02c510b17c5308f941b3c81ec58e5b6448dcc78
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-12-20 14:48:59 -0800

    Make a Package struct

--- src/package.rs
+++ src/package.rs
@@ -1,7 +1,23 @@
 use super::debug;
-use yaml_rust::{Yaml, YamlLoader};
+use serde::{Deserialize, Serialize};
 
-pub fn get_package_version(_package: &str) -> &str {
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct InnerPackage {
+    pub name: Option<String>,
+    pub description: Option<String>,
+    pub version: Option<String>,
+    pub url: Option<String>,
+    pub license: Option<String>,
+    pub authors: Option<Vec<String>>,
+    pub depends: Option<Vec<String>>,
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct Package {
+    pub package: InnerPackage,
+}
+
+pub fn get_package_version(package: &str) -> &str {
     // TODO: get package version from name
     return "Not 0.0.1";
 }
@@ -12,15 +28,20 @@ pub fn get_package_url(_package: &str) -> &str {
 }
 
 // Convert this to object oriented at some point
-pub fn fetch_package_manifest(package: &str) -> Result<Vec<Yaml>, ()> {
+pub fn fetch_package_manifest(package: &str) -> Result<Package, serde_yaml::Error> {
     let uri = format!(
         "https://raw.githubusercontent.com/{}/main/package.yml",
         package
     );
 
     debug!("Pulling manifest from {}", uri);
-    let req = reqwest::get(&uri).unwrap().text();
-    Ok(YamlLoader::load_from_str(&req.expect("Couldn't get manifest")).unwrap())
+    let req = reqwest::get(&uri).unwrap().text().unwrap();
+    debug!("{}", req);
+
+    let package_yaml: Package = serde_yaml::from_str(&req)?;
+    debug!("{:?}", package_yaml);
+
+    Ok(package_yaml)
 }
 
 #[cfg(test)]
@@ -28,20 +49,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn fetch_package_manifest_test() -> Result<(), ()> {
+    fn fetch_package_manifest_test() -> Result<(), serde_yaml::Error> {
+        let pack: Package = fetch_package_manifest("JakeRoggenbuck/yf-package-example")?;
+        assert_eq!(pack.package.name, Some("yf-package-example".to_string()));
         assert_eq!(
-            fetch_package_manifest("JakeRoggenbuck/yf-package-example")?[0]["package"]["name"]
-                .as_str()
-                .unwrap(),
-            "yf-package-example"
+            pack.package.description,
+            Some("An example package for yflat".to_string())
         );
-
-        assert_ne!(
-            fetch_package_manifest("JakeRoggenbuck/yf-package-example")?[0]["package"]["name"]
-                .as_str()
-                .unwrap(),
-            "yf-package"
-        );
+        assert_eq!(pack.package.version, Some("0.1.0".to_string()));
+        assert_eq!(pack.package.url, Some("https://github.com/JakeRoggenbuck/yf-package-example".to_string()));
+        assert_eq!(pack.package.authors, Some(vec!["Jake Roggenbuck".to_string()]));
+        assert_eq!(pack.package.license, Some("MIT".to_string()));
+        assert_eq!(pack.package.depends, None);
 
         Ok(())
     }
