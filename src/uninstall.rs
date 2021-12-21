b7a097d045e4f42196ae6b49121513d6bd6c3071
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-12-20 16:42:00 -0800

    Add check before removing a non existent dir

--- src/uninstall.rs
+++ src/uninstall.rs
@@ -2,12 +2,20 @@ use super::debug;
 use super::package::get_local_dir;
 use super::spinner::spinner;
 use std::fs;
+use std::path::Path;
 
 pub fn uninstall(package: &str) -> std::io::Result<()> {
     let spin = spinner("Uninstalling...".to_string());
 
     let local = get_local_dir();
-    fs::remove_dir_all(format!("{}/{}", local, package))?;
+    let package_name = format!("{}/{}", local, package);
+
+    if !Path::new(&package_name).exists() {
+        eprintln!("Package {} does not exist", package,);
+        std::process::exit(0);
+    }
+
+    fs::remove_dir_all(package_name)?;
     debug!("Successfully removed package");
 
     spin.finish_with_message("Done ✔");
