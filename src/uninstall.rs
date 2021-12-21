d18687381e7634cd5c233303f42dd68f88b74c7c
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-12-20 16:23:40 -0800

    Add new spinner to uninstall, done

--- src/uninstall.rs
+++ src/uninstall.rs
@@ -1,25 +1,16 @@
 use super::debug;
 use super::package::get_local_dir;
-use indicatif::{ProgressBar, ProgressStyle};
+use super::spinner::spinner;
 use std::fs;
 
 pub fn uninstall(package: &str) -> std::io::Result<()> {
-    let pb = ProgressBar::new_spinner();
-    pb.enable_steady_tick(120);
-    pb.set_style(
-        ProgressStyle::default_spinner()
-            .tick_strings(&[
-                "⠄", "⠆", "⠇", "⠋", "⠙", "⠸", "⠰", "⠠", "⠰", "⠸", "⠙", "⠋", "⠇", "⠆",
-            ])
-            .template("{spinner:.blue} {msg}"),
-    );
-    pb.set_message("Uninstalling...");
+    let spin = spinner("Uninstalling...".to_string());
 
     let local = get_local_dir();
     fs::remove_dir_all(format!("{}/{}", local, package))?;
     debug!("Successfully removed package");
 
-    pb.finish_with_message("Done ✔");
+    spin.finish_with_message("Done ✔");
 
     Ok(())
 }
