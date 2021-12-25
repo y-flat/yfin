e96bcdc19996dc9890936d7fea80bcb451f19c6d
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-12-24 22:19:51 -0800

    Add error handle

--- src/repository.rs
+++ src/repository.rs
@@ -1,71 +1,67 @@
 use super::debug;
-use super::error;
+use super::error::Error;
 use super::package::get_local_dir;
 use std::collections::HashMap;
+use std::fs::File;
+use std::io;
 use std::io::{Read, Write};
 use std::path::Path;
 
-pub fn update_repository_cache() -> Result<(), error::Error> {
-    let uri = format!(
-        "https://raw.githubusercontent.com/Camerooooon/yfin-official-repository/master/packages"
-    );
-    let mut req = reqwest::get(&uri).unwrap();
+const YFIN_OFFICIAL_REPOSITORY: &str =
+    "https://raw.githubusercontent.com/Camerooooon/yfin-official-repository/master/packages";
 
-    if !req.status().is_success() {
-        return Err(error::Error::RequestFailed);
-    }
+pub fn update_repository_cache() -> Result<(), io::Error> {
+    let mut req = match reqwest::get(YFIN_OFFICIAL_REPOSITORY) {
+        Ok(r) => r,
+        Err(..) => std::process::exit(1),
+    };
 
     let text = req.text().unwrap();
 
-    debug!("{}", text);
-
     let local = get_local_dir();
     let path = Path::new(&local).join("packages");
-    println!("{:?}", path);
+    debug!("{:?}", path);
 
-    match std::fs::File::create(path) {
-        Ok(mut reference) => match reference.write_all(text.as_bytes()) {
-            Ok(_) => return Ok(()),
-            Err(_) => {
-                return Err(error::Error::FileSystemError);
-            }
-        },
-        Err(e) => panic!("{}", e),
-    };
+    let mut reference = File::create(path)?;
+    reference.write_all(text.as_bytes())?;
+
+    Ok(())
 }
 
-pub fn get_official_repositories() -> Result<HashMap<String, String>, error::Error> {
+pub fn get_official_repositories() -> Result<HashMap<String, String>, io::Error> {
     let local = get_local_dir();
     let path = Path::new(&local).join("packages");
 
     if !path.exists() {
-        println!("You have not downloaded the official repository list, some packages may be missing. Download it with yfin update\n\n");
+        println!(
+            "You have not downloaded the official repository list, some packages may be missing.\
+Download it with yfin update\n\n"
+        );
     }
 
-    match std::fs::File::open(path) {
-        Ok(mut file) => {
-            let mut text = String::new();
-            file.read_to_string(&mut text);
-            let mut package: HashMap<String, String> = HashMap::new();
-            for line in text.lines() {
-                let vals: Vec<&str> = line.trim().split(":").collect();
-                package.insert(vals[1].to_string(), vals[0].to_string());
-            }
-            Ok(package)
-        }
-        Err(_) => return Err(error::Error::FileSystemError),
+    let mut file = File::open(path)?;
+    let mut text = String::new();
+    file.read_to_string(&mut text)?;
+
+    let mut package: HashMap<String, String> = HashMap::new();
+
+    for line in text.lines() {
+        let vals: Vec<&str> = line.trim().split(":").collect();
+        package.insert(vals[1].to_string(), vals[0].to_string());
     }
+
+    Ok(package)
 }
 
-pub fn get_official_url(name: &str) -> Result<String, error::Error> {
+pub fn get_official_url(name: &str) -> Result<String, Error> {
     match get_official_repositories() {
         Ok(repository) => {
             let package = repository.get(name);
             if package == None {
-                return Err(error::Error::PackageNotFoundError);
+                return Err(Error::PackageNotFoundError);
             }
             return Ok(package.unwrap().to_string());
         }
-        Err(_) => return Err(error::Error::FileSystemError),
+        Err(_) => return Err(Error::FileSystemError),
     }
 }
