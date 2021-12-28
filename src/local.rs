f998c35af6c76d30a06d41fd0c2a11a8316c413e
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-12-28 13:42:35 -0800

    Add check and create .yflat/bin

--- src/local.rs
+++ src/local.rs
@@ -2,16 +2,16 @@ use super::debug;
 use std::fs;
 use std::path::Path;
 
-pub fn check_for_local() -> bool {
+pub fn check_for_local(path_name: &str) -> bool {
     match home::home_dir() {
         Some(path) => {
             let local_path =
-                Path::new(format!("{}/.local/lib/yflat/", path.display()).as_str()).to_owned();
+                Path::new(format!("{}/{}/", path.display(), &path_name).as_str()).to_owned();
 
             if local_path.exists() {
                 return true;
             } else {
-                debug!("Could not find ~/.local/lib/yflat/");
+                debug!("Could not find ~/{}", path_name);
             }
         }
         None => println!("Could not get home directory"),
@@ -20,11 +20,11 @@ pub fn check_for_local() -> bool {
     return false;
 }
 
-pub fn create_local() {
+pub fn create_local(path_name: &str) {
     match home::home_dir() {
         Some(path) => {
             let local_path =
-                Path::new(format!("{}/.local/lib/yflat/", path.display()).as_str()).to_owned();
+                Path::new(format!("{}/{}", path.display(), &path_name).as_str()).to_owned();
 
             match fs::create_dir_all(local_path.clone()) {
                 Ok(_) => debug!("Created {:?}", local_path),
@@ -33,4 +33,20 @@ pub fn create_local() {
         }
         None => println!("Could not get home directory"),
     }
+}
+
+pub fn check_for_local_lib() -> bool {
+    check_for_local(".local/lib/yflat/")
+}
+
+pub fn create_local_lib() {
+    create_local(".local/lib/yflat");
+}
+
+pub fn check_for_local_bin() -> bool {
+    check_for_local(".yflat/bin/")
+}
+
+pub fn create_local_bin() {
+    create_local(".yflat/bin/")
 }
