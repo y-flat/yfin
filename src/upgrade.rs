1c683ba256a6462b77a2200fcbac501135508eb7
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-02-03 17:28:03 -0800

    Extract upgrade

--- src/upgrade.rs
+++ src/upgrade.rs
@@ -3,19 +3,26 @@ use crate::package::get_local_dir;
 use std::env;
 use std::process::Command;
 
-pub fn upgrade(name: &str) -> std::io::Result<()> {
-    let spin = spinner("Upgrading...".to_string());
-
-    let local = get_local_dir();
-    let package_path = format!("{}/{}", local, name);
-    env::set_current_dir(package_path)?;
-
+fn git_pull() {
     Command::new("git")
         .arg("pull")
         .output()
         .expect("Failed to pull");
+}
+
+fn go_to_package_dir(name: &str) -> std::io::Result<()> {
+    let local = get_local_dir();
+    let package_path = format!("{}/{}", local, name);
+    env::set_current_dir(package_path)?;
+    Ok(())
+}
+
+pub fn upgrade(name: &str) -> std::io::Result<()> {
+    let spin = spinner("Upgrading...".to_string());
+
+    go_to_package_dir(name)?;
+    git_pull();
 
     spin.finish_with_message("Done ✔");
-
     Ok(())
 }
